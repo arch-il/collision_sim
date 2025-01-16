05ce18c9c8f9ad7305716d708c91f95ded9275ed
arch_il <archilberaia+github@gmail.com>
2025-01-16 19:33:52 +0400

    add frame time graphs

--- src/database.rs
+++ src/database.rs
@@ -3,6 +3,7 @@ use macroquad::{
     math::Vec2,
     shapes::{draw_line, draw_rectangle_lines},
     text::draw_text,
+    time,
 };
 
 use crate::simulation::Simulation;
@@ -15,8 +16,8 @@ pub struct Database {
     kinetic_energy: [f32; GRAPH_SIZE],
     potential_energy: [f32; GRAPH_SIZE],
     mechanical_energy: [f32; GRAPH_SIZE],
-    // frame_time: [f32; GRAPH_SIZE],
-    // simulation_time: [f32; GRAPH_SIZE],
+    frame_time: [f32; GRAPH_SIZE],
+    simulation_time: [f32; GRAPH_SIZE],
     index: usize,
     // energy_enabed: bool,
     // frame_time_enabled: bool,
@@ -29,8 +30,8 @@ impl Database {
             kinetic_energy: [0.0; GRAPH_SIZE],
             potential_energy: [0.0; GRAPH_SIZE],
             mechanical_energy: [0.0; GRAPH_SIZE],
-            // frame_time: [0.0; GRAPH_SIZE],
-            // simulation_time: [0.0; GRAPH_SIZE],
+            frame_time: [0.0; GRAPH_SIZE],
+            simulation_time: [0.0; GRAPH_SIZE],
             index: 0,
             // energy_enabed: true,
             // frame_time_enabled: true,
@@ -55,6 +56,10 @@ impl Database {
         self.mechanical_energy[self.index] =
             self.kinetic_energy[self.index] + self.potential_energy[self.index];
 
+        self.frame_time[self.index] = time::get_frame_time();
+
+        self.simulation_time[self.index] = simulation_time;
+
         self.index += 1;
         if self.index >= GRAPH_SIZE {
             self.index = 0;
@@ -64,6 +69,11 @@ impl Database {
     pub fn input(&mut self) {}
 
     pub fn draw(&self) {
+        self.draw_energy();
+        self.draw_frame_time();
+    }
+
+    fn draw_energy(&self) {
         let curr_index = if self.index == 0 {
             GRAPH_SIZE - 1
         } else {
@@ -138,6 +148,80 @@ impl Database {
                 RECT.1 + RECT.3 - self.mechanical_energy[i + 1] * energy_scale,
                 1.0,
                 color::PURPLE,
+            );
+        }
+    }
+
+    fn draw_frame_time(&self) {
+        const ID: i32 = 1;
+        const TITLE_RECT: (f32, f32, f32, f32) = (
+            500.0 + GAP,
+            125.0 * ID as f32 + GAP * (2 * ID + 1) as f32,
+            GRAPH_SIZE as f32,
+            25.0,
+        );
+        draw_rectangle_lines(
+            TITLE_RECT.0,
+            TITLE_RECT.1,
+            TITLE_RECT.2,
+            TITLE_RECT.3,
+            RECT_THICKNESS,
+            color::YELLOW,
+        );
+        draw_text(
+            &format!(
+                "Frame Time - {}",
+                self.frame_time.iter().sum::<f32>() / GRAPH_SIZE as f32
+            ),
+            TITLE_RECT.0 + GAP,
+            TITLE_RECT.1 + 17.0,
+            23.0,
+            color::LIGHTGRAY,
+        );
+
+        const RECT: (f32, f32, f32, f32) = (
+            500.0 + GAP,
+            125.0 * ID as f32 + GAP * (2 * ID + 2) as f32 + 25.0,
+            GRAPH_SIZE as f32,
+            100.0,
+        );
+        draw_rectangle_lines(
+            RECT.0,
+            RECT.1,
+            RECT.2,
+            RECT.3,
+            RECT_THICKNESS,
+            color::YELLOW,
+        );
+
+        let frame_scale = 75.0
+            / self
+                .frame_time
+                .iter()
+                .max_by(|a, b| a.partial_cmp(b).unwrap())
+                .unwrap();
+
+        for i in 0..(GRAPH_SIZE - 1) {
+            if i + 1 == self.index {
+                continue;
+            }
+
+            draw_line(
+                RECT.0 + i as f32,
+                RECT.1 + RECT.3 - self.frame_time[i] * frame_scale,
+                RECT.0 + (i + 1) as f32,
+                RECT.1 + RECT.3 - self.frame_time[i + 1] * frame_scale,
+                1.0,
+                color::YELLOW,
+            );
+
+            draw_line(
+                RECT.0 + i as f32,
+                RECT.1 + RECT.3 - self.simulation_time[i] * frame_scale,
+                RECT.0 + (i + 1) as f32,
+                RECT.1 + RECT.3 - self.simulation_time[i + 1] * frame_scale,
+                1.0,
+                color::LIME,
             );
         }
     }
